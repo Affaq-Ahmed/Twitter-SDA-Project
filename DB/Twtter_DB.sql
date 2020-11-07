821a90fab34ee42406c1878a09dc6a2ec31edd44
qasim1101 <74052088+qasim1101@users.noreply.github.com>
2020-11-07 15:53:51 +0500

    Add files via upload

---
+++ DB/Twtter_DB.sql
@@ -0,0 +1,28 @@
+create database Twitter
+use Twitter
+
+
+Create Table Users
+(
+name nvarchar(100),
+userid nvarchar(100) NOT NULL PRIMARY KEY,
+password nvarchar(30),
+)
+go
+
+Create Table Followed
+(
+followerid nvarchar(100) FOREIGN KEY REFERENCES Users(userid),
+followingid nvarchar(100) FOREIGN KEY REFERENCES Users(userid),
+CONSTRAINT pk_Follower PRIMARY KEY (followerid,followingid)
+)
+go
+
+Create Table Blocked
+(
+userid nvarchar(100) FOREIGN KEY REFERENCES Users(userid),
+blockedid nvarchar(100) FOREIGN KEY REFERENCES Users(userid)
+CONSTRAINT pk_BlockedUser PRIMARY KEY (userid,blockedid)
+)
+go
+
